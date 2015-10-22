27cc9690c57903c32d6d9e5964c5731b30a54512
Hao Zhe (Houser) <hzk11a@acu.edu>
2015-10-22 03:02:13 -0500

    Added plant functionality

--- app/views.py
+++ app/views.py
@@ -1,14 +1,22 @@
 from flask import render_template, flash, redirect, session, url_for,request
-from app import app
+from app import app, db, models
 from .forms import LoginForm
 
 @app.route("/")
 def index():
 	return 'Index Page'
 
-@app.route("/map")
+@app.route("/map", methods=['GET', 'POST'])
 def map():
-	return render_template('map.html')
+	if request.method == 'POST':
+		print(request.form['commonName'])
+		p = models.Plant(commonName = request.form['commonName'], latinName = request.form['latinName'], latitude = request.form['latitude'], longitude = request.form['longitude'])
+		db.session.add(p)
+		db.session.commit()
+	p = models.Plant.query.all()
+	plants = [dict(id=row.id, commonName=row.commonName, latinName=row.latinName, latitude=row.latitude, longitude=row.longitude) for row in p]
+	print plants
+	return render_template('map.html', plants = plants)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
