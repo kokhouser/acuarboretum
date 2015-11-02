beaf598424c4eb86bf62e1f74f59e24b325a8bb4
Hao Zhe (Houser) <hzk11a@acu.edu>
2015-11-01 23:42:39 -0600

    Added admin editing capabilities

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for,request
 from app import app, db, models
 from .forms import LoginForm
+from sqlalchemy.sql import exists
 
 @app.route("/")
 def index():
@@ -9,13 +10,32 @@ def index():
 @app.route("/map", methods=['GET', 'POST'])
 def map():
 	if request.method == 'POST':
-		print(request.form['commonName'])
-		p = models.Plant(commonName = request.form['commonName'], latinName = request.form['latinName'], latitude = request.form['latitude'], longitude = request.form['longitude'])
-		db.session.add(p)
-		db.session.commit()
+		#print (db.session.query(exists().where(models.Plant.longitude == request.form['longitude'])).scalar())
+		#print (request.form['ide'])
+		if db.session.query(exists().where(models.Plant.id == request.form['ide'])).scalar():
+			#print (db.session.query(exists().where(models.Plant.id == request.form['ide'])).scalar())
+			p = db.session.query(models.Plant).\
+				filter(models.Plant.id==request.form['ide'])
+			if 'delete' in request.form and request.form['delete'] == 'delete':
+				print ("Here!")
+				for i in p:
+					print (i)
+					db.session.delete(i)
+					db.session.commit()
+			else:
+				for i in p:
+				 	print (i)
+				 	i.commonName = request.form['commonName']
+				 	i.latinName = request.form['latinName']
+				 	db.session.commit()
+		else:
+			p = models.Plant(commonName = request.form['commonName'], latinName = request.form['latinName'], latitude = request.form['latitude'], longitude = request.form['longitude'])
+			db.session.add(p)
+			db.session.commit()
 	p = models.Plant.query.all()
 	plants = [dict(id=row.id, commonName=row.commonName, latinName=row.latinName, latitude=row.latitude, longitude=row.longitude) for row in p]
-	print plants
+	# Debug statement
+	#print plants
 	return render_template('map.html', plants = plants)
 
 @app.route('/login', methods=['GET', 'POST'])
